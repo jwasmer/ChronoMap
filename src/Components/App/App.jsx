b30c085b281482fbe561072737328e684bd4b84e
James Wasmer <jameswasmer@gmail.com>
2023-01-11 16:20:11 -0500

    Build file framework for map

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  const [] = useState()
 
+  const [user, setUser] = useState(false)
+
+  // The "App" level exists to leave room for a user login path at the top level. I don't want to place Map logic in here because I plan to add user authentication after this assignment is turned in, and leaving App.jsx available to coordinate that process will keep my code cleaner in the long run.
   return (
-    
+    <>
+      <Map />
+    </>
   );
 }
 
