03ebc0f5b88445aee1a6cd772fedff367b3155de
James Wasmer <jameswasmer@gmail.com>
2023-01-13 12:21:01 -0500

    Build searchbar skeleton

--- src/Components/MapUI/MapUI.jsx
+++ src/Components/MapUI/MapUI.jsx
@@ -1,8 +1,16 @@
+import React, { useState, useEffect } from 'react'
 import Map from '../Map/Map'
+import './MapUI.css'
 
 export default function MapUI() {
-
+  const [search, setSearch] = useState('')
+  
   return (
-    <Map />
+    <>
+      <Map />
+      <div className='searchbar'>
+        <input type='text' name='searchbar' value={search} onChange={event => setSearch(event.target.value)} />
+      </div>
+    </>
   )
 }
