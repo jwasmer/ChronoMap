792aa1855499b1859e75ab2a09114808f058d0b0
James Wasmer <jameswasmer@gmail.com>
2023-01-13 01:04:06 -0500

    Import API calls

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -2,22 +2,22 @@ import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-load
 import React, { ReactDOM, useState, useEffect, useRef } from 'react';
 import './Map.css'
 import { featureCollectionTemplate, featureTemplate, symbolLayer, buildFeature, updateFeatureCollection } from './MapboxHelpers';
+import { reverseGeocode, forwardGeocode, geocodeQuery } from '.../apiCalls/Geocode'
+import { isochroneQuery } from '../../apiCalls/Isochrone';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiandhc21lciIsImEiOiJjbGNwbjFiNjI3bnBiM3FwOWFyYnZyNmRtIn0.dy0DAO9j8qhnJ-df-xb1Yw' // how to hide this?
+mapboxgl.accessToken = 'pk.eyJ1Ijoiandhc21lciIsImEiOiJjbGNwbjFiNjI3bnBiM3FwOWFyYnZyNmRtIn0.dy0DAO9j8qhnJ-df-xb1Yw'
 
 export default function Map() {
-  const [coordinates, setCoordinates] = useState([])
-  const [feature, setFeature] = useState(null)
-  const [geoJson, setGeoJson] = useState(featureCollection)
+  const [coordinates, setCoordinates] = useState(0)
+  const [featureCollection, setFeatureCollection] = useState(featureCollectionTemplate)
+  const [marker, setMarker] = useState(null)
 
-  // defines default values for map
   const mapContainer = useRef(null)
     const map = useRef(null)
     const [lng, setLng] = useState(-70.9)
     const [lat, setLat] = useState(42.35)
     const [zoom, setZoom] = useState(9)
 
-  // ensures map isn't rendered before the map's container has been created
   useEffect(() => {
     if (map.current) return;
 
@@ -29,49 +29,51 @@ export default function Map() {
     })
 
     map.current.on('load', () => {
-      map.current.loadImage('.../assets/marker-purple.png', (error, image) => {
-        if (error) throw error
-
-        map.current.addImage('marker', image)
-      })
-
-      map.addSource('points', featureCollectionTemplate)
+      map.current.addSource('points', featureCollection)
       map.current.addLayer(symbolLayer)
+    })
+
+    map.current.on('click', (event) => {
+      setCoordinates(event.lngLat)
     })
   })
 
-  // stores new lat/long/zoom values when user interacts with a map
   useEffect(() => {
     if (!map.current) return;
 
-    map.current.on('move', () => { // on() method attaches event handlers for the selected elements and child elements
-      setLng(map.current.getCenter().lng.toFixed(4)) // getCenter() returns map geographical centerpoint {lng: 0, lat: 0}
-      setLat(map.current.getCenter().lat.toFixed(4)) // toFixed() defines number of digits following a decimal point, here it's 4 digits (0.0000)
-      setZoom(map.current.getZoom().toFixed(2)) // getZoom() returns the map's current zoom level as a number
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4))
+      setLat(map.current.getCenter().lat.toFixed(4))
+      setZoom(map.current.getZoom().toFixed(2))
     })
   })
 
-  // creates a single event listener to pull lat/long coordinates from the map on click
-  const getCoordinates = async () => {
-    map.current.once('click', (event) => {
-      setCoordinates(event.lngLat)
-    })
-  }
+  useEffect(() => {
+    if (marker) marker.remove()
 
-  const saveCoordinates = () => {
+    if (coordinates) {
+      const newMarker = new mapboxgl.Marker()
+      newMarker.setLngLat([coordinates.lng, coordinates.lat]).addTo(map.current)
+      setMarker(newMarker)
+    }
+  }, [coordinates])
 
-    setGeoJson([...geoJson, coordinates])
+  const saveCoordinates = (featureTemplate, coordinates) => {
+    const features = featureCollection.data.features
+
+    const collection = updateFeatureCollection(featureCollectionTemplate, features, buildFeature(featureTemplate, coordinates))
+
+    setFeatureCollection(collection)
+    console.log(featureCollection)
   }
 
   return (
     <div>
       <div className='sidebar'>
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        Longitude: {coordinates.lng} | Latitude: {coordinates.lat} | Zoom: {zoom}
       </div>
       <button onClick={saveCoordinates}>Save Coordinates</button>
-      <div ref={mapContainer} className="map-container" onClick={(event) => {
-        getCoordinates(event)
-      }}/>
+      <div ref={mapContainer} className="map-container" />
 
     </div>
   )
