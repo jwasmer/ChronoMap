f239910acf62c5d4c026515aa070eb8a45417b03
James Wasmer <jameswasmer@gmail.com>
2023-01-13 01:03:25 -0500

    Build calls for geocode/isochrone APIs

---
+++ src/apiCalls/Isochrone.js
@@ -0,0 +1,17 @@
+export const isochroneQuery = async (profile='driving', coordinates, time='60') => {
+  const query = `https://api.mapbox.com/isochrone/v1/mapbox/${profile}/${coordinates}?contours_minutes=${time}&polygons=true&access_token=pk.eyJ1Ijoiandhc21lciIsImEiOiJjbGNwbjFiNjI3bnBiM3FwOWFyYnZyNmRtIn0.dy0DAO9j8qhnJ-df-xb1Yw`
+
+  try {
+    const response = fetch(query)
+
+    if (!response.ok) {
+      throw new Error(`Status code: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } 
+  catch (error) {
+    console.log(error)
+  }
+}
