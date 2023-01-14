ad93a37ce43badb1c7c03084748d0c40d24e4de5
James Wasmer <jameswasmer@gmail.com>
2023-01-14 16:52:39 -0500

    Add save functionality to favorites button

--- src/Components/MapUI/MapUI.jsx
+++ src/Components/MapUI/MapUI.jsx
@@ -10,30 +10,80 @@ import IconButton from '@mui/material/IconButton';
 import { forwardGeocode, geocodeQuery } from '../../apiCalls/Geocode'
 import TimeMenu from '../Menu/TimeMenu.jsx'
 import IconMenu from '../Menu/IconMenu.jsx'
+import { featureCollectionTemplate } from '../Map/MapboxHelpers';
 
 export default function MapUI() {
   const [searchInput, setSearchInput] = useState('')
   const [searchGeoJson, setSearchGeoJson] = useState(null)
   const [time, setTime] = useState('60')
   const [profile, setProfile] = useState('car')
-  
+  const [savedAddresses, setSavedAddresses] = useState([])
+  const [currentPolygon, setCurrentPolygon] = useState(null)
+  const [count, setCount] = useState(1)
+
+  const saveCurrentPolygon = (currentPolygon, count) => {
+    setCount(count + 1)
+    setSavedAddresses([...savedAddresses, currentPolygon])
+  }
+
   return (
     <main>
-      <Map searchGeoJson={ searchGeoJson }/>
+      <Map  
+        setSearchGeoJson={ setSearchGeoJson } 
+        setCurrentPolygon={ setCurrentPolygon } 
+        searchGeoJson={ searchGeoJson }
+        profile={profile} 
+        time={time} 
+        currentPolygon={ currentPolygon }
+        count={ count }
+      />
       <form className='searchbar'>
-        <input background-color='white' className='search-input' type='text' name='searchbar' placeholder='Search for address' value={searchInput} onChange={event => setSearchInput(event.target.value)} />
-        <IconButton type='submit' size='large' color="primary" aria-label="search picture" component="label" onClick={() => {setSearchGeoJson(geocodeQuery(forwardGeocode(searchInput)))}}>
+        <input 
+          background-color='white' 
+          className='search-input' 
+          type='text' name='searchbar' 
+          placeholder='Search for address' 
+          value={searchInput} 
+          onChange={event => setSearchInput(event.target.value)} 
+        />
+        <IconButton 
+          type='submit' 
+          size='large' 
+          color="primary" 
+          aria-label="search picture" 
+          component="label" 
+          onClick={() => {setSearchGeoJson(geocodeQuery(forwardGeocode(searchInput)))}}
+        >
           <SearchTwoToneIcon />
         </IconButton>
         <div className='divider'></div>
-        <IconButton id='favorite' size='large' color="primary" aria-label="search picture" component="label">
+        <IconButton 
+          id='favorite' 
+          size='large' 
+          color="primary" 
+          aria-label="search picture" 
+          component="label" 
+          onClick={() => {saveCurrentPolygon(currentPolygon, count)}}
+        >
           <FavoriteTwoToneIcon />
         </IconButton>
       </form>
-      <IconMenu />
-      <TimeMenu />
+      <IconMenu 
+        setProfile={ setProfile } 
+      />
+      <TimeMenu 
+        setTime={ setTime } 
+      />
       <div className='button-container'>
-        <Button variant="contained" size='large' component={Link} to='/options'>VIEW SAVED</Button>
+        <Button 
+          variant="contained" 
+          size='large' 
+          component={Link} to='/options'
+          savedAddresses={ savedAddresses }
+          setSavedAddresses={ setSavedAddresses }
+        >
+          VIEW SAVED
+        </Button>
       </div>
     </main>
   )
