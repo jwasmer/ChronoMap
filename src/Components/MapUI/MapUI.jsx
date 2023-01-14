fa4406d803bb01c4b5716669b1675b04fe1d155d
James Wasmer <jameswasmer@gmail.com>
2023-01-14 01:38:53 -0500

    Build map UI component and pass geocoded search to map as props

--- src/Components/MapUI/MapUI.jsx
+++ src/Components/MapUI/MapUI.jsx
@@ -5,28 +5,31 @@ import './MapUI.css'
 import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
 import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 import IconButton from '@mui/material/IconButton';
+import { forwardGeocode, geocodeQuery } from '../../apiCalls/Geocode'
+import TimeMenu from '../Menu/TimeMenu.jsx'
+import IconMenu from '../Menu/IconMenu.jsx'
 
 export default function MapUI() {
-  const [search, setSearch] = useState('')
-  
-  const runSearch = (search, coordinates) => {
-    
-  }
+  const [searchInput, setSearchInput] = useState('')
+  const [searchGeoJson, setSearchGeoJson] = useState(null)
+  const [time, setTime] = useState('60')
+  const [profile, setProfile] = useState('car')
 
   return (
     <main>
-      <Map runSearch={runSearch} search={search}/>
-      <div className=''></div>
+      <Map searchGeoJson={ searchGeoJson }/>
       <form className='searchbar'>
-        <input className='search-input' type='text' name='searchbar' placeholder='Search for address' value={search} onChange={event => setSearch(event.target.value)} />
-        <IconButton type='submit' size='large' color="primary" aria-label="search picture" component="label" onClick={runSearch}>
+        <input background-color='white' className='search-input' type='text' name='searchbar' placeholder='Search for address' value={searchInput} onChange={event => setSearchInput(event.target.value)} />
+        <IconButton type='submit' size='large' color="primary" aria-label="search picture" component="label" onClick={() => {setSearchGeoJson(geocodeQuery(forwardGeocode(searchInput)))}}>
           <SearchTwoToneIcon />
         </IconButton>
         <div className='divider'></div>
-        <IconButton size='large' color="primary" aria-label="search picture" component="label">
+        <IconButton id='favorite' size='large' color="primary" aria-label="search picture" component="label">
           <FavoriteTwoToneIcon />
         </IconButton>
       </form>
+      <IconMenu />
+      <TimeMenu />
       <div className='button-container'>
         <Button variant="contained" size='large'>VIEW SAVED</Button>
       </div>
