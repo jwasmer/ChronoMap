9d759f0fb4fbca8338d3de3cf4da4df8a2423d61
James Wasmer <jameswasmer@gmail.com>
2023-01-14 14:24:02 -0500

    Hardcode layout for Options page

--- src/Components/Options/Options.jsx
+++ src/Components/Options/Options.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react'
+import './Options.css'
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Address from '../Address/Address';
 
-export default function Storage({ geojson }) {
+export default function Options() {
 
-  
+
+  return(
+    <div className='background'>
+      <div className='header'>
+        <Typography variant='h4'>Options</Typography>
+        <div className='button-container'>
+          <Button variant="contained" size='large' component={Link} to='/'>VIEW MAP</Button>
+        </div>
+      </div>
+      <Address />
+      <Address />
+    </div>
+  )
 }
