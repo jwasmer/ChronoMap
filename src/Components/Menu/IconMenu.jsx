c9a7a2ee95b353ec2bb48c24602a06eada030737
James Wasmer <jameswasmer@gmail.com>
2023-01-14 16:53:10 -0500

    Remove public transit option from menu

--- src/Components/Menu/IconMenu.jsx
+++ src/Components/Menu/IconMenu.jsx
@@ -13,7 +13,7 @@ import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
 import DirectionsSubwayIcon from '@mui/icons-material/DirectionsSubway';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 
-export default function IconMenu() {
+export default function IconMenu({ setProfile }) {
   const [anchorEl, setAnchorEl] = useState(null);
   const [selection, setSelection] = useState(<DirectionsCarIcon/>)
   const open = Boolean(anchorEl)
@@ -77,25 +77,28 @@ export default function IconMenu() {
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <MenuItem onClick={() => setSelection(<DirectionsWalkIcon/>)}>
+        <MenuItem onClick={() => {
+          setSelection(<DirectionsWalkIcon/>)
+          setProfile('walking')
+          }}>
           <ListItemIcon>
             <DirectionsWalkIcon fontSize="small" />
           </ListItemIcon>
           <ListItemText>Walk</ListItemText>
         </MenuItem>
-        <MenuItem onClick={() => {setSelection(<DirectionsBikeIcon/>)}}>
+        <MenuItem onClick={() => {
+          setSelection(<DirectionsBikeIcon/>)
+          setProfile('cycling')
+          }}>
           <ListItemIcon>
             <DirectionsBikeIcon fontSize="small" />
           </ListItemIcon>
           <ListItemText>Bike</ListItemText>
         </MenuItem>
-        <MenuItem onClick={() => {setSelection(<DirectionsSubwayIcon/>)}}>
-          <ListItemIcon>
-            <DirectionsSubwayIcon fontSize="small" />
-          </ListItemIcon>
-          <ListItemText>Public</ListItemText>
-        </MenuItem>
-        <MenuItem onClick={() => {setSelection(<DirectionsCarIcon/>)}}>
+        <MenuItem onClick={() => {
+          setSelection(<DirectionsCarIcon/>)
+          setProfile('driving')
+          }}>
           <ListItemIcon>
             <DirectionsCarIcon fontSize="small" />
           </ListItemIcon>
