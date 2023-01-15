38c5ad56a0a385a3d0d4cc0f49512a9e3de0e098
James Wasmer <jameswasmer@gmail.com>
2023-01-14 21:30:28 -0500

    Receive and unpack props from MapUI

--- src/Components/Options/Options.jsx
+++ src/Components/Options/Options.jsx
@@ -1,21 +1,31 @@
 import './Options.css'
 import { Button, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 import Address from '../Address/Address';
+import { useLocation } from 'react-router-dom';
 
 export default function Options() {
+  const location = useLocation()
+  const savedAddresses = location.savedAddresses
+  const setSavedAddresses = location.setSavedAddresses
 
+  console.log(location)
+
+  console.log(savedAddresses)
 
   return(
     <div className='background'>
       <div className='header'>
         <Typography variant='h4'>Options</Typography>
         <div className='button-container'>
-          <Button variant="contained" size='large' component={Link} to='/'>VIEW MAP</Button>
+          <Button variant="contained" size='large' component={Link} to='/map/'>VIEW MAP</Button>
         </div>
       </div>
-      <Address />
-      <Address />
+      {}
+      <Address 
+        // setSavedAddresses={ setSavedAddresses }
+        // savedAddresses={ savedAddresses }
+      />
     </div>
   )
 }
