842d6f7c113fac57cd19949a3ef111932ae9d8f5
James Wasmer <jameswasmer@gmail.com>
2023-01-15 23:42:54 -0500

    Build logic for time buttons

--- src/Components/Address/TimeButtons.jsx
+++ src/Components/Address/TimeButtons.jsx
@@ -1,22 +1,99 @@
+import { useState, useEffect } from "react";
+import { Radio, FormControl, RadioGroup, FormControlLabel, Avatar } from '@mui/material'
+import '../Options/Options.css'
+
 export default function TimeButtons({ saveData, setSaveData }) {
-  // const [selection, setSelection] = useState(saveData.foreign.time)
-    const updateButtons = () => {
-      console.log(this.id)
-    }
+  console.log('TimeButton saveData:', saveData[0])
+  const [value, setValue] = useState(60)
+  // saveData[0].foreign.time
+
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  useEffect(() => {
+    console.log('value:', value)
+    setSaveData((prevState) => {
+      return {
+      ...prevState,
+        [0]: {
+          ...prevState[0],
+            foreign: {
+              ...prevState[0].foreign,
+                time: value
+          }
+        }
+      }
+    })
+  }, [value])
+
   return (
-    <FormControl>
-      <IconButton id='15' onClick={updateButtons}>
-        <Avatar sx={{width: 48, height: 48, bgcolor: '#8e24aa', opacity: '.87'}}>15m</Avatar>
-      </IconButton>
-      <IconButton id='30'>
-        <Avatar sx={{width: 48, height: 48}}>30m</Avatar>
-      </IconButton>
-      <IconButton id='15'>
-        <Avatar sx={{width: 48, height: 48}}>45m</Avatar>
-      </IconButton>
-      <IconButton id='60'>
-        <Avatar sx={{width: 48, height: 48}}>60m</Avatar>
-      </IconButton>
-    </FormControl>
+    <>
+      <FormControl>
+        <RadioGroup
+          row
+          defaultValue="60"
+          value={value}
+          name="profile-selection-buttons"
+          onChange={handleChange}
+        >
+          <FormControlLabel 
+            value={15} 
+            control={
+              <Radio 
+                size="large" 
+                icon={<Avatar sx={{width: 48, height: 48}}> 15m </Avatar>} 
+                checkedIcon={<Avatar sx={{width: 48, height: 48, bgcolor: '#8e24aa'}}> 15m </Avatar>} 
+              />
+            } 
+          />
+          <FormControlLabel 
+            value={30}
+            control={
+              <Radio 
+                size="large" 
+                icon={<Avatar sx={{width: 48, height: 48}}> 30m </Avatar>} 
+                checkedIcon={<Avatar sx={{width: 48, height: 48, bgcolor: '#8e24aa'}}> 30m </Avatar>} 
+              />
+            } 
+          />          
+          <FormControlLabel 
+            value={45} 
+            control={
+              <Radio 
+                size="large" 
+                icon={<Avatar sx={{width: 48, height: 48}}> 45m </Avatar>} 
+                checkedIcon={<Avatar sx={{width: 48, height: 48, bgcolor: '#8e24aa'}}> 45m </Avatar>} 
+              />
+            } 
+          />          
+          <FormControlLabel 
+            value={60}
+            control={
+              <Radio 
+                size="large" 
+                icon={<Avatar sx={{width: 48, height: 48}}> 60m </Avatar>} 
+                checkedIcon={<Avatar sx={{width: 48, height: 48, bgcolor: '#8e24aa'}}> 60m </Avatar>} 
+              />
+            } 
+          />          
+        </RadioGroup>
+      </FormControl>
+    </>
   )
 }
+
+
+{/* <IconButton id='15' onClick={updateButtons}>
+<Avatar sx={{width: 48, height: 48}}>15m</Avatar>
+</IconButton>
+<IconButton id='30'>
+<Avatar sx={{width: 48, height: 48}}>30m</Avatar>
+</IconButton>
+<IconButton id='15'>
+<Avatar sx={{width: 48, height: 48}}>45m</Avatar>
+</IconButton>
+<IconButton id='60'>
+<Avatar sx={{width: 48, height: 48}}>60m</Avatar>
+</IconButton> */}
